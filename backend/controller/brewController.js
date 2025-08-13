94527326fcde861993d09a2640545a7cd0ed8839
sdSAHABUDDIN <syedsahabuddin182@gmail.com>
2025-08-13 09:46:55 +0530

    Add GitHub Pages deploy Workflow

--- backend/controller/brewController.js
+++ backend/controller/brewController.js
@@ -1,34 +1,36 @@
 export const brewCoffee=async(req,res)=>{
 
 }
-let currentStep="Idle";//global variable
+
+let currentStep = "Idle";
+let isCancelled = false;
+
 export const setBrewStep = (req, res) => {
   const { step } = req.body;
   console.log(`Received step: ${step}`);
-  currentStep=step;
-  res.json({message:"step updated"})
 
-  // Example logic
-  // switch (step) {
-  //   case 'CONNECT_TO_ESP32':
-  //     brewStatus = 'Connecting to ESP32...';
-  //     break;
-  //   case 'START_BREWING':
-  //     brewStatus = 'Brewing...';
-  //     break;
-  //   case 'WAIT_FOR_CUP':
-  //     brewStatus = 'Waiting for cup...';
-  //     break;
-  //   case 'TAKE_COFFEE':
-  //     brewStatus = 'Coffee ready, take it!';
-  //     break;
-  //   default:
-  //     brewStatus = 'Idle';
-  // }
+  // Reset cancel flag on new brewing cycle
+  if (step === "Connecting to ESP32") {
+    isCancelled = false;
+  }
 
-  // res.json({ status: brewStatus });
+  // If cancellation happened previously, block further steps
+  if (isCancelled) {
+    console.log("Ignored step due to cancellation:", step);
+    return res.status(400).json({ message: "Brewing cancelled" });
+  }
+
+  // If user clicked Done or Cancel
+  if (step === "Idle") {
+    isCancelled = true;
+    currentStep = "Idle";
+    return res.json({ message: "Stopped" });
+  }
+
+  currentStep = step;
+  res.json({ message: "Step updated" });
 };
 
-export const getBrewStatus=async(req,res)=>{
-  res.json({ status: currentStep});
-}
+export const getBrewStatus = async (req, res) => {
+  res.json({ status: currentStep });
+};
